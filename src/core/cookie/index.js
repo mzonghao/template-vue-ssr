89a7a0dfcd595ec02ceeb4a1a0e0e4b8a0291f62
mzonghao <846862233@qq.com>
2018-11-04 00:12:10 +0800

    Set cookie method

--- src/core/cookie/index.js
+++ src/core/cookie/index.js
@@ -4,4 +4,10 @@ export default class Cookie {
     const arr = document.cookie.match(reg);
     return arr ? unescape(arr[2]) : '';
   }
+
+  set(name, value, days) {
+    const exp = new Date();
+    exp.setTime(exp.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${escape(value)};expires=${exp.toGMTString()}`;
+  }
 }
